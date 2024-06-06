3c36a7af09d26b3f49d99d236c7854bc6620fd8f
OleksandrTverdoi <alextverdoy@gmail.com>
2024-06-06 20:06:54 +0300

    minor fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App';
 import theme from './theme';
 
